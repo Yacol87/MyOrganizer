f06982a51b374504eaa68618fc43e506b7919c1d
Jacek Bukała <bukala.jacek@gmail.com>
2026-02-19 11:18:16 +0100

    first commit

---
+++ domain/src/entities/User.java
@@ -0,0 +1,13 @@
+package entities;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
